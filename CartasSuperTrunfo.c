3dc21f5ff9be231621e763c86fbbd0cd0283d82d
Jusdip7 <196688999+Jusdip7@users.noreply.github.com>
2025-02-18 16:37:16 +0000

    Consegui fazer a comparação funcionar

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,51 +6,6 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
-//Uma função para cadastrar a segunda carta
-void cadastroCartas() {
-    char codigo2[10];
-    char nome2[50];
-    int populacao2;
-    float pib2;
-    float area2;
-    int pontosTuristicos2;
-    float densidade2;
-    float PIBpercapita2;
-
-    printf("Insira o código da cidade:\n");
-    scanf("%s", codigo2);
-    printf("Insira o nome da cidade:\n");
-    scanf("%s", nome2);
-    printf("Insira a população da cidade:\n");
-    scanf("%d", &populacao2);
-    printf("Insira a área da cidade:\n");
-    scanf("%f", &area2);
-    printf("Insira o PIB da cidade:\n");
-    scanf("%f", &pib2);
-    printf("Insira o número de pontos turísticos da cidade:\n");
-    scanf("%d", &pontosTuristicos2);
-
-    densidade2 = populacao2 / area2;
-    PIBpercapita2 = pib2 / populacao2;
-
-    printf("\n*** Dados da Carta-%s***\n", codigo2);
-    printf("Código da cidade: %s\n", codigo2);
-    printf("Nome da cidade: %s\n", nome2);
-    printf("População da cidade: %d\n", populacao2);
-    printf("Área da cidade: %.2fkm²\n", area2);
-    printf("PIB da cidade: R$%.2f\n", pib2);
-    printf("Número de pontos turísticos da cidade: %d\n", pontosTuristicos2);
-    printf("Densidade populacional: %.2f hab/km²\n", densidade2);
-    printf("PIB per capita: R$%.2f\n", PIBpercapita2);
-
-
-}
-
-//Função de comparaçao de cartas. Como a farei? Sei la, só fé.
-void compararCartas() {
-    
-}
-
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
@@ -69,6 +24,8 @@ int main() {
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    printf("*** Cadastro da Primeira Carta ***\n");
+    
     printf("Insira o código da cidade:\n");
     scanf("%s", codigo);
     printf("Insira o nome da cidade:\n");
@@ -100,18 +57,130 @@ int main() {
     printf("PIB per capita: R$%.2f\n", PIBpercapita);
 
     //Cadastro da segunda carta
-    char opcao[5];
-    printf("Deseja cadastrar outra carta? (s/n)\n");
-    scanf("%s", &opcao);
 
-    if (opcao[0] == 's' || opcao[0] == 'S') {
-        cadastroCartas();
+    char codigo2[10];
+    char nome2[50];
+    int populacao2;
+    float pib2;
+    float area2;
+    int pontosTuristicos2;
+    float densidade2;
+    float PIBpercapita2;
+
+    printf("\n*** Cadastro da Segunda Carta ***\n");
+
+    printf("Insira o código da cidade:\n");
+    scanf("%s", codigo2);
+    printf("Insira o nome da cidade:\n");
+    scanf("%s", nome2);
+    printf("Insira a população da cidade:\n");
+    scanf("%d", &populacao2);
+    printf("Insira a área da cidade:\n");
+    scanf("%f", &area2);
+    printf("Insira o PIB da cidade:\n");
+    scanf("%f", &pib2);
+    printf("Insira o número de pontos turísticos da cidade:\n");
+    scanf("%d", &pontosTuristicos2);
+
+    densidade2 = populacao2 / area2;
+    PIBpercapita2 = pib2 / populacao2;
+
+    printf("\n*** Dados da Carta-%s***\n", codigo2);
+    printf("Código da cidade: %s\n", codigo2);
+    printf("Nome da cidade: %s\n", nome2);
+    printf("População da cidade: %d\n", populacao2);
+    printf("Área da cidade: %.2fkm²\n", area2);
+    printf("PIB da cidade: R$%.2f\n", pib2);
+    printf("Número de pontos turísticos da cidade: %d\n", pontosTuristicos2);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB per capita: R$%.2f\n", PIBpercapita2);
+
+
+    // Comparação das Cartas:
+    printf("\n*** Comparação das Cartas ***\n");
+    int carta1 = 0, carta2 = 0;
+
+    printf("Carta %s- População: %d\n", codigo, populacao);
+    printf("Carta %s- População: %d\n", codigo2, populacao2);
+    if (populacao > populacao2) {
+        printf("A carta %s tem a maior população que a carta %s\n +1 ponto pra carta %s\n", codigo, codigo2, codigo);
+        carta1++;
+    } else if (populacao < populacao2) {
+        printf("A carta %s tem a maior população que a carta %s\n +1 ponto pra carta %s\n", codigo2, codigo, codigo2);
+        carta2++;
     } else {
-        printf("Cadastro encerrado.\n");
+            printf("As cartas %s e %s têm a mesma população\n", codigo, codigo2);
     }
 
-    //Comparação de cartas (Tentarei. Conseguirei?)
+    printf("Carta %s- Área: %.2fkm²\n", codigo, area);
+    printf("Carta %s- Área: %.2fkm²\n", codigo2, area2);
+    if (area > area2) {
+        printf("A carta %s tem a maior área que a carta %s\n +1 ponto pra carta %s\n", codigo, codigo2, codigo);
+        carta1++;
+    } else if (area < area2) {
+        printf("A carta %s tem a maior área que a carta %s\n +1 ponto pra carta %s\n", codigo2, codigo, codigo2);
+        carta2++;
+    } else {
+            printf("As cartas %s e %s têm a mesma área\n", codigo, codigo2);
+    }  
+
+    printf("Carta %s- PIB: R$:%.2f\n", codigo, pib);
+    printf("Carta %s- PIB: R$:%.2f\n", codigo2, pib2);
+    if (pib > pib2) {
+        printf("A carta %s tem o maior PIB que a carta %s\n +1 ponto pra carta %s\n", codigo, codigo2, codigo);
+        carta1++;
+    } else if (pib < pib2) {
+        printf("A carta %s tem o maior PIB que a carta %s\n +1 ponto pra carta %s\n", codigo2, codigo, codigo2);
+        carta2++;
+    } else {
+            printf("As cartas %s e %s têm o mesmo PIB\n", codigo, codigo2);
+    }
     
+    printf("Carta %s- Número de pontos turísticos: %d\n", codigo, pontosTuristicos);
+    printf("Carta %s- Número de pontos turísticos: %d\n", codigo2, pontosTuristicos2);
+    if (pontosTuristicos > pontosTuristicos2) {
+        printf("A carta %s tem mais pontos turísticos que a carta %s\n +1 ponto pra carta %s\n", codigo, codigo2, codigo);
+        carta1++;
+    } else if (pontosTuristicos < pontosTuristicos2) {
+        printf("A carta %s tem mais pontos turísticos que a carta %s\n +1 ponto pra carta %s\n", codigo2, codigo, codigo2);
+        carta2++;
+    } else {
+            printf("As cartas %s e %s têm o mesmo número de pontos turísticos\n", codigo, codigo2);
+    }
+
+    printf("Carta %s- Densidade Populacional: %.2fhab/km²\n", codigo, Densidade);
+    printf("Carta %s- Densidade Populacional: %.2fhab/km²\n", codigo2, densidade2);
+    printf("Obs: Ganha a carta com menor densidade populacional\n");
+    if (Densidade > densidade2) {
+        printf("A carta %s tem a maior densidade populacional que a carta %s\n +1 ponto pra carta %s\n", codigo, codigo2, codigo2);
+        carta2++;
+    } else if (Densidade < densidade2) {
+        printf("A carta %s tem a maior densidade populacional que a carta %s\n +1 ponto pra carta %s\n", codigo2, codigo, codigo);
+        carta1++;
+    } else {
+            printf("As cartas %s e %s têm a mesma densidade\n", codigo, codigo2);
+    }
+
+    printf("Carta %s- PIB per capita: R$:%.2f\n", codigo, PIBpercapita);
+    printf("Carta %s- PIB per capita: R$:%.2f\n", codigo2, PIBpercapita2);
+    if (PIBpercapita > PIBpercapita2) {
+        printf("A carta %s tem o maior PIB per capita que a carta %s\n +1 ponto pra carta %s\n", codigo, codigo2, codigo);
+        carta1++;
+    } else if (PIBpercapita < PIBpercapita2) {
+        printf("A carta %s tem o maior PIB per capita que a carta %s\n +1 ponto pra carta %s\n", codigo2, codigo, codigo2);
+        carta2++;
+    } else {
+            printf("As cartas %s e %s têm o mesmo PIB per capita\n", codigo, codigo2);
+    }
+
+    printf("\n*** Resultado da Comparação ***\n");
+    if (carta1 > carta2) {
+        printf("A carta %s venceu com %d pontos!\n", codigo, carta1);
+    } else if (carta1 < carta2) {
+        printf("A carta %s venceu com %d pontos!\n", codigo2, carta2);
+    } else if (carta1 == carta2) {
+        printf("As cartas %s e %s empataram!\n", codigo, codigo2);
+    }
 
     return 0;
 }
